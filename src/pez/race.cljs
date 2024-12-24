f9d59ea88d409c21964e24e4f7cdb26f7af7a263
PEZ <pez@pezius.com>
2024-12-24 23:55:36 +0100

    Make drawing resizable

--- src/pez/race.cljs
+++ src/pez/race.cljs
@@ -7,7 +7,7 @@
    [quil.middleware :as m]
    [replicant.dom :as d]))
 
-(def frame-rate 120)
+(def frame-rate 180)
 (def start-time-line-x-% 0.2)
 
 (def max-start-time (->> bd/benchmarks
@@ -27,77 +27,81 @@
              (- (benchmark lang) (:start-time lang)))
            (:languages bd/benchmarks)))
 
-(defn get-viewport
-  "Returns a vector of [vw vh] for the current browser window, scaled by the scale parameter.
-   If true is passed to force-square the smaller dimension will be used on both axes."
-  [scale]
-  [(* scale (.-innerWidth js/window)) (* scale (.-innerHeight js/window))])
+(defn dims []
+  [(min 800 (- (.-innerWidth js/window) 20)) (+ 80 (* 50 (count (:languages bd/benchmarks))))])
 
-(def dims [(min 800 (- (.-innerWidth js/window) 20)) (+ 80 (* 50 (count (:languages bd/benchmarks))))])
-
-(defn setup [benchmark]
-  (def benchmark benchmark)
-  (q/frame-rate frame-rate)
-  (q/image-mode :center)
-
-  (let [start-time-line-x (* (q/width) start-time-line-x-%)
-        total-starting-sequence-ticks (* frame-rate 2)
-        total-track-ticks (/ frame-rate 1)
-        ball-width 40
+(defn arena [width height]
+  (let [ball-width 40
         half-ball-width (/ ball-width 2)
+        start-time-line-x (* width start-time-line-x-%)
         start-line-x (+ start-time-line-x half-ball-width 5)
-        finish-line-x (- (q/width) half-ball-width 5)
-        track-length (- finish-line-x start-line-x)
-        max-time (apply max (benchmark-times benchmark))
-        min-time (apply min (benchmark-times benchmark))]
-    {:t 0
-     :benchmark benchmark
+        finish-line-x (- width half-ball-width 5)]
+    {:width width
+     :ball-width ball-width
+     :half-ball-width half-ball-width
      :start-time-line-x start-time-line-x
      :start-line-x start-line-x
      :finish-line-x finish-line-x
-     :track-length track-length
-     :middle-x (/ (q/width) 2)
-     :middle-y (/ (q/height) 2)
-     :ball-width ball-width
-     :half-ball-width half-ball-width
-     :race-started false
-     :max-start-time max-start-time
-     :total-starting-sequence-ticks total-starting-sequence-ticks
-     :total-track-ticks total-track-ticks
-     :max-time max-time
-     :min-time min-time
-     :start-message "Starting engines!"
-     :race-message "github.com/bddicken/languages"
-     :greeting-timeout (* frame-rate 2)
-     :languages (mapv (fn [i {:keys [start-time] :as lang}]
-                        (let [starting-sequence-ticks (* (/ start-time max-start-time)
-                                                         total-starting-sequence-ticks)
-                              benchmark-time (- (benchmark lang) start-time)
-                              speed (/ min-time benchmark-time)
-                              track-ticks (/ total-track-ticks speed)]
-                          (merge lang
-                                 {:speed speed
-                                  :runs 0
-                                  :direction 1
-                                  :track-ticks track-ticks
-                                  :track-x-per-tick (/ track-length track-ticks)
-                                  :track-x start-line-x
-                                  :starting-sequence-ticks starting-sequence-ticks
-                                  :start-sequence-x-per-tick (/ start-time-line-x starting-sequence-ticks)
-                                  :start-sequence-x 0
-                                  :hello-world-shown false
-                                  :greeting nil
-                                  :benchmark-time (- (benchmark lang) start-time)
-                                  :benchmark-time-str (str (.toFixed benchmark-time 1) " ms")
-                                  :x 0
-                                  :y (+ 70 (* i 50))
-                                  :logo-image (q/load-image (:logo lang))})))
-                      (range)
-                      (languages benchmark))}))
+     :track-length (- finish-line-x start-line-x)
+     :middle-x (/ width 2)
+     :middle-y (/ height 2)
+     }))
 
-(defn update-state [{:keys [race-started greeting-timeout start-time-line-x total-starting-sequence-ticks start-line-x finish-line-x ] :as draw-state}]
-  (let [t (inc (:t draw-state))]
+
+(defn setup [benchmark]
+  (q/frame-rate frame-rate)
+  (q/image-mode :center)
+
+  (let [total-starting-sequence-ticks (* frame-rate 2)
+        total-track-ticks (/ frame-rate 1)
+        arena (arena (q/width) (q/height))
+        {:keys [start-time-line-x start-line-x track-length]} arena
+        max-time (apply max (benchmark-times benchmark))
+        min-time (apply min (benchmark-times benchmark))]
+    (merge arena
+           {:t 0
+            :benchmark benchmark
+            :race-started false
+            :max-start-time max-start-time
+            :total-starting-sequence-ticks total-starting-sequence-ticks
+            :total-track-ticks total-track-ticks
+            :max-time max-time
+            :min-time min-time
+            :start-message "Starting engines!"
+            :race-message "github.com/bddicken/languages"
+            :greeting-timeout (* frame-rate 2)
+            :languages (mapv (fn [i {:keys [start-time] :as lang}]
+                               (let [starting-sequence-ticks (* (/ start-time max-start-time)
+                                                                total-starting-sequence-ticks)
+                                     benchmark-time (- (benchmark lang) start-time)
+                                     speed (/ min-time benchmark-time)
+                                     track-ticks (/ total-track-ticks speed)]
+                                 (merge lang
+                                        {:speed speed
+                                         :runs 0
+                                         :direction 1
+                                         :track-ticks track-ticks
+                                         :track-x-per-tick (/ track-length track-ticks)
+                                         :track-x start-line-x
+                                         :starting-sequence-ticks starting-sequence-ticks
+                                         :start-sequence-x-per-tick (/ start-time-line-x starting-sequence-ticks)
+                                         :start-sequence-x 0
+                                         :hello-world-shown false
+                                         :greeting nil
+                                         :benchmark-time (- (benchmark lang) start-time)
+                                         :benchmark-time-str (str (.toFixed benchmark-time 1) " ms")
+                                         :x 0
+                                         :y (+ 70 (* i 50))
+                                         :logo-image (q/load-image (:logo lang))})))
+                             (range)
+                             (languages benchmark))})))
+
+(defn update-state [{:keys [race-started greeting-timeout total-starting-sequence-ticks] :as draw-state}]
+  (let [arena (arena (q/width) (q/height))
+        {:keys [start-time-line-x start-line-x finish-line-x]} arena
+        t (inc (:t draw-state))]
     (merge draw-state
+           arena
            {:t t}
            (when (> t (+ total-starting-sequence-ticks (/ frame-rate 10)))
              {:race-started true})
@@ -117,7 +121,7 @@
                                               new-direction (if bounce?
                                                               (* -1 direction)
                                                               direction)]
-                                          {:track-x new-x
+                                          {:track-x (min (max new-x start-line-x) finish-line-x)
                                            :direction new-direction
                                            :runs (if bounce? (inc runs) runs)}))))
                              (:languages draw-state))})))
@@ -134,11 +138,12 @@
             track-x (:track-x lang)
             runs (:runs lang)]
         (q/text-style :normal)
-        (q/fill 100)
+        (q/fill 120)
         (q/rect 0 (- y 10) (q/width) 20)
         (q/text-align :right :center)
-        (q/fill 40)
-        (q/rect (:start-sequence-x lang) (- y 10) (- start-time-line-x (:start-sequence-x lang)) 20)
+        (when-not race-started
+          (q/fill 60)
+          (q/rect (:start-sequence-x lang) (- y 10) (- start-time-line-x (:start-sequence-x lang)) 20))
         (q/fill "white")
         ;(q/text-style :bold)
         (q/text-size 14)
@@ -166,15 +171,17 @@
           (q/text (:race-message draw-state) middle-x 20))))))
 
 (defn run-sketch [benchmark]
-  (q/sketch
-    :host "race"
-    :size dims
-    :renderer :p2d
-    :setup (fn [] (setup benchmark))
-    :update update-state
-    :draw draw-state!
+  (def benchmark benchmark)
+  (set! quil.sketch/*applet*
+        (q/sketch
+         :host "race"
+         :size (dims)
+         :renderer :p2d
+         :setup (fn [] (setup benchmark))
+         :update update-state
+         :draw draw-state!
     ;; :key-pressed (u/save-image "export.png")
-    :middleware [m/fun-mode]))
+         :middleware [m/fun-mode])))
 
 (defn app [state]
   (def state state)
@@ -205,11 +212,13 @@
   (let [{:keys [new-state effects]}
         (reduce (fn [{state :new-state :as acc} action]
                   (js/console.debug "Triggered action" action)
-                  (let [[action-name & args] (enrich-action-from-replicant-data replicant-data action)
-                        {:keys [new-state effects]} (cond
-                                                      (= :app/set-benchmark action-name) (let [benchmark (keyword (first args))]
-                                                                                           {:new-state (assoc state :benchmark benchmark)
-                                                                                            :effects [[:draw/run-sketch benchmark]]}))]
+                  (let [[action-name & args]
+                        (enrich-action-from-replicant-data replicant-data action)
+                        {:keys [new-state effects]}
+                        (cond
+                          (= :app/set-benchmark action-name) (let [benchmark (keyword (first args))]
+                                                               {:new-state (assoc state :benchmark benchmark)
+                                                                :effects [[:draw/run-sketch benchmark]]}))]
                     (cond-> acc
                       new-state (assoc :new-state new-state)
                       effects (update :effects into effects))))
@@ -241,9 +250,16 @@
   (inspector/inspect "App state" !state)
   (add-watch !state :update (fn [_k _r _o n]
                               (render-app! app-el n)))
+  (js/window.addEventListener "resize" (fn [_e]
+                                         (let [[w h] (dims)]
+                                           (q/resize-sketch w h))))
   (d/set-dispatch! event-handler)
   (start)
   (run-sketch (:benchmark @!state)))
+
+(comment
+  quil.sketch/*applet*
+  :rcf)
 
 ;; this is called before any code is reloaded
 (defn ^:dev/before-load stop []
