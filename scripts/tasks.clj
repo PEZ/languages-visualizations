8c1ed7b809617b510345d7d00a885fe79f13dfe0
PEZ <pez@pezius.com>
2024-12-30 00:54:44 +0100

    Add bench tasks

--- scripts/tasks.clj
+++ scripts/tasks.clj
@@ -1,6 +1,7 @@
 (ns tasks
   (:require
    [babashka.fs :as fs]
+   [babashka.process :as p]
    [cheshire.core :as json]
    [clojure.pprint :as pprint]
    [clojure.string :as string]))
@@ -24,12 +25,39 @@
                  (assoc-in acc [language benchmark] mean))
                {})))
 
+(defn compile! [languages-dir]
+  (doseq [benchmark ["hello-world" "levenshtein" "loops" "fibonacci"]]
+    (println "COMPILE:" benchmark)
+    (p/shell {:dir (fs/path languages-dir benchmark)
+              :continue true}
+             "../compile.sh")))
+
+(defn bench! [languages-dir]
+  (let [scripts-dir(-> (System/getProperty "babashka.config")
+                       fs/parent
+                       (fs/path "scripts"))
+        bench-script (str (fs/path scripts-dir "bench.sh"))]
+    (println scripts-dir bench-script)
+    (doseq [benchmark ["levenshtein" "loops" "fibonacci"]]
+      (println "BENCH:" benchmark)
+      (println bench-script)
+      (p/shell {:dir (fs/path languages-dir benchmark)
+                :continue true}
+               bench-script))))
+
 (defn ^:export collect-benchmark-data! [& args]
   (pprint/pprint
    (get-benchmark-means-from-path! (or (first args) "/tmp/languages"))))
 
+(defn ^:export compile-benchmarks! [& args]
+  (compile! (or (first args) "../languages")))
+
+(defn ^:export bench-benchmarks! [& args]
+  (println args)
+  (bench! (or (first args) "../languages")))
+
 (comment
   (get-benchmark-means-from-path! "/tmp/languages")
-  (map str (fs/glob "/tmp/languages" "*/*.json"))
+  (get-benchmark-means-from-path! "/Volumes/Macintosh HD-1/tmp/languages")
+  (compile! "../../languages")
   :rcf)
-
