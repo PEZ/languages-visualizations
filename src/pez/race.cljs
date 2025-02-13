f93533d2ec1909580ae2d95622c53fd9886a449f
PEZ <pez@pezius.com>
2025-02-13 21:07:41 +0100

    WIP: Use projectiles that go back to base instead of bounce

--- src/pez/race.cljs
+++ src/pez/race.cljs
@@ -24,48 +24,87 @@
 (def language-labels-x 200)
 (def ball-width 44)
 (def half-ball-width (/ ball-width 2))
-(def start-line-x (+ language-labels-x half-ball-width 10))
+(def start-line-x (+ language-labels-x 10))
+(def logo-start-line-x (+ start-line-x half-ball-width))
+(def projectile-lifetime 2500)
 
 (defn dims [{:keys [app-el] :as app-state}]
   [(min drawing-width (.-offsetWidth app-el)) (+ 100 (* 45 (count (benchmark/best-languages < app-state))))])
 
 (defn arena [width height]
-  (let [finish-line-x (- width half-ball-width 5)]
+  (let [logo-finish-line-x (- width half-ball-width 5)
+        track-length (- logo-finish-line-x logo-start-line-x)]
     {:width width
-     :finish-line-x finish-line-x
-     :track-length (- finish-line-x start-line-x)
+     :logo-finish-line-x logo-finish-line-x
+     :track-length track-length
+     :finish-line-x (+ start-line-x track-length)
      :middle-x (/ width 2)
      :middle-y (/ height 2)}))
 
 
 (defn update-draw-state [draw-state app-state now]
-  (let [arena (arena (q/width) (q/height))
-        paused? (:paused? app-state)
+  (let [arena               (arena (q/width) (q/height))
+        track-length        (:track-length arena)
+        paused?             (:paused? app-state)
         manual-display-time (:manual-display-time app-state)
-        display-time (if paused?
-                       (or manual-display-time 0)
-                       (t->display-time app-state now))
-        elapsed-ms (display-time->elapsed-ms app-state display-time)]
+        display-time        (if paused?
+                              (or manual-display-time 0)
+                              (t->display-time app-state now))
+        elapsed-ms          (display-time->elapsed-ms app-state display-time)
+
+        updated-languages
+        (mapv (fn [{:keys [speed] :as lang}]
+                (let [normalized  (double (/ elapsed-ms (:min-track-time-ms app-state)))
+                      scaled-time (* normalized speed)
+                      distance    (* track-length scaled-time)]
+                  (merge lang
+                         (if (>= distance track-length)
+                           {:x    (+ logo-start-line-x track-length)
+                            :runs (long (quot distance track-length))}
+                           {:x    (+ logo-start-line-x distance)
+                            :runs (long (quot distance track-length))}))))
+              (:languages draw-state))
+
+        computed-projectiles
+        (mapcat
+         (fn [lang]
+           (let [speed (:speed lang)
+                 ms-per-run (double (/ (:min-track-time-ms app-state) speed))
+                 completed-runs (:runs lang)
+                 projectile-lifetime (/ projectile-lifetime speed)
+                 i-min (long (Math/floor
+                              (max 1 (/ (- elapsed-ms projectile-lifetime)
+                                        ms-per-run))))
+                 i-max completed-runs]
+             (for [i (range i-min (inc i-max))]
+               (let [run-completion-ms (* i ms-per-run)
+                     dt (- elapsed-ms run-completion-ms)]
+                 (when (and (>= dt 0)
+                            (< dt projectile-lifetime))
+                   (let [dt                  (- elapsed-ms run-completion-ms)
+                         fraction           (/ dt projectile-lifetime)
+                         wave-factor        (* 2 speed)
+                         phase              (* i 1.2)
+                         wave-amplitude     10
+                         wave               (Math/sin (+ (* fraction 2 Math/PI wave-factor)
+                                                         phase))
+                         wave-offset        (* wave wave-amplitude)
+                         x (q/lerp (:finish-line-x arena)
+                                   start-line-x
+                                   fraction)
+                         y (+ (:y lang) wave-offset)]
+                     {:x     x
+                      :y     y
+                      :color (:color lang)}))))))
+         updated-languages)]
+
     (merge draw-state
            arena
-           {:t (t->elapsed-ms app-state now)
+           {:t                (t->elapsed-ms app-state now)
             :display-time-str (.toFixed display-time 7)
-            :app-state app-state
-            :languages
-            (mapv (fn [{:keys [speed] :as lang}]
-                    (merge lang
-                           (let [normalized (/ elapsed-ms (:min-track-time-ms app-state))
-                                 scaled-time (* normalized speed)
-                                 distance (* (:track-length arena) scaled-time)
-                                 loop-distance (mod distance (* 2 (:track-length arena)))
-                                 x (if (> loop-distance (:track-length arena))
-                                     (- (* 2 (:track-length arena)) loop-distance)
-                                     loop-distance)]
-                             {:x (+ start-line-x x)
-                              :runs (quot distance (:track-length arena))
-                              ;:done? (> distance (:track-length arena))
-                              })))
-                  (:languages draw-state))})))
+            :app-state        app-state
+            :languages        updated-languages
+            :projectiles      (vec (keep identity computed-projectiles))})))
 
 (defn setup [{:keys [benchmark add-overlaps? max-time min-time] :as app-state}]
   (q/frame-rate 120)
@@ -82,14 +121,14 @@
                                      speed (/ min-time speed-mean)]
                                  (merge lang
                                         {:speed speed
-                                         :color (str (:color lang) "33")
+                                         :bar-color (str (:color lang) "33")
                                          :runs 0
                                          :speed-ratio (/ max-time min-time mean)
                                          :greeting "Hello, World!"
                                          :benchmark-time mean
                                          :benchmark-time-str (str (-> mean (.toFixed 2)))
                                          :std-dev-str (str (-> stddev (.toFixed 2)))
-                                         :x start-line-x
+                                         :x logo-start-line-x
                                          :y (+ 110 (* i 45))
                                          :logo-image (q/load-image (:logo lang))})))
                              (range)
@@ -110,13 +149,12 @@
      :button-w w
      :button-h h}))
 
-;; filepath: /Users/pez/Projects/drag-race-visualization/src/pez/race.cljs
-(defn render-languages [{:keys [track-length] :as draw-state}]
+(defn render-languages! [{:keys [track-length] :as draw-state}]
   (q/text-size 14)
   (let [paused? (get-in draw-state [:app-state :paused?])]
     (doseq [lang (:languages draw-state)]
-      (let [{:keys [language-name color logo-image x y runs benchmark-time-str std-dev-str speed]} lang]
-        (q/fill color)
+      (let [{:keys [language-name bar-color logo-image x y runs benchmark-time-str std-dev-str speed]} lang]
+        (q/fill bar-color)
         (q/rect (+ language-labels-x 5) (- y 12) (* speed track-length) 24)
         (q/fill darkgrey)
         (q/rect 0 (- y 12) (+ language-labels-x 5) 24)
@@ -134,8 +172,13 @@
             (q/stroke-weight 0))
           (q/fill black))
         (q/text benchmark-time-str language-labels-x (- y 20))
-        (when-not (:done? lang)
-          (q/image logo-image x y ball-width ball-width))))))
+        (q/image logo-image x y ball-width ball-width)))))
+
+(defn render-projectiles! [{:keys [projectiles]}]
+  (doseq [{:keys [x y color]} projectiles]
+    (q/fill color)
+    (q/no-stroke)
+    (q/ellipse (- x 4) y 8 8)))
 
 (defn draw! [{:keys [benchmark-title middle-x width display-time-str] :as draw-state}]
   (q/background offwhite)
@@ -153,7 +196,8 @@
   (q/text "±" (- language-labels-x 80) 65)
   (q/text "ms" language-labels-x 65)
   (q/text display-time-str (- width 5) 65)
-  (render-languages draw-state))
+  (render-projectiles! draw-state)
+  (render-languages! draw-state))
 
 (defn save-image! [{:keys [benchmark filter-champions?]}]
   (let [iso-date (.substring (.toISOString (js/Date.)) 0 10)]
@@ -163,8 +207,6 @@
                  (when-not filter-champions? "-all")
                  ".png"))))
 
-;; filepath: /Users/pez/Projects/drag-race-visualization/src/pez/race.cljs
-;; filepath: /Users/pez/Projects/drag-race-visualization/src/pez/race.cljs
 (defn run-sketch! [event-handler]
   (set! quil.sketch/*applet*
         (q/sketch
